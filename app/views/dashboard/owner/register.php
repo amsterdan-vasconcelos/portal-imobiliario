0abe3455142894921f97b235b32aff0447a9d232
Amsterdan Vasconcelos <amsterdan.email@gmail.com>
2025-08-29 13:17:23 -0300

    fix: correct inputs in the owner registration form

--- app/views/dashboard/owner/register.php
+++ app/views/dashboard/owner/register.php
@@ -22,27 +22,35 @@
 
       <form class="c-form c-form--dashboard">
         <div class="c-form__item">
-          <label class="c-label" for="">Nome</label>
-          <input class="c-input c-input--dashboard" type="text">
+          <label class="c-label" for="name">Nome</label>
+          <input
+            class="c-input c-input--dashboard"
+            type="text" id="name" name="name">
         </div>
         <div class="c-form__item">
-          <label class="c-label" for="">Contato</label>
-          <input class="c-input c-input--dashboard" type="text">
+          <label class="c-label" for="phone">Telefone</label>
+          <input
+            class="c-input c-input--dashboard"
+            type="tel" id="phone" name="phone">
         </div>
 
         <fieldset class="c-fieldset">
           <legend class="c-fieldset__legend">Gênero</legend>
           <div class="c-fieldset__item">
-            <label class="c-label" for="">Femino</label>
-            <input type="radio" name="" id="">
+            <label class="c-label" for="F">Femino</label>
+            <input type="radio" name="gender" id="F">
           </div>
           <div class="c-fieldset__item">
-            <label class="c-label" for="">Masculino</label>
-            <input type="radio" name="" id="">
+            <label class="c-label" for="M">Masculino</label>
+            <input type="radio" name="gender" id="M">
           </div>
         </fieldset>
 
-        <button class="c-button c-button--dashboard c-button--1/2" type="submit">Adicionar</button>
+        <button
+          class="c-button c-button--dashboard c-button--1/2"
+          type="submit">
+          Adicionar
+        </button>
       </form>
 
     </div>
